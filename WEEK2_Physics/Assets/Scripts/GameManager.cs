41981847171c873c47b1b6cdd52b19b5bc58ad8a
Hilary Chan <hilary0310@gmail.com>
2019-09-07 13:33:32 -0400

    95%

--- WEEK2_Physics/Assets/Scripts/GameManager.cs
+++ WEEK2_Physics/Assets/Scripts/GameManager.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -9,17 +10,36 @@ public class GameManager : MonoBehaviour
 
     public int score;
     public Text Score;
+    public Canvas Instruct;
+    public Canvas ScoreShow;
+    public GameObject spawn1;
+    public string sceneName;
+
 
     // Start is called before the first frame update
     void Start()
     {
         Instance = this;
         score = 0;
+        Instruct.GetComponent<Canvas>().enabled = true;
+        ScoreShow.GetComponent<Canvas>().enabled = false;
+        spawn1.SetActive(false);
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instruct.GetComponent<Canvas>().enabled = false;
+            ScoreShow.GetComponent<Canvas>().enabled = true;
+            spawn1.SetActive(true);
+
+        }
+
+
+
         if (score > 9)
         {
            Score.text = "0" + score.ToString();
@@ -38,6 +58,10 @@ public class GameManager : MonoBehaviour
             //u win
         }
     }
-
+    public void Menu()
+    {
+        SceneManager.LoadScene(sceneName);
     }
+
+}
 
---
+++ WEEK2_Physics/Assets/Scripts/StartSceneManager.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StartSceneManager : MonoBehaviour
+{
+    public Canvas StartMenu;
+    public Canvas instruct;
+    [SerializeField] public GameObject particleDot;
+    [SerializeField] public Image Title;
+    [SerializeField] public Text Starts;
+
+    Animator Anim1;
+    Animator Anim2;
+    Animator Anim3;
+
+
+
+    void Awake()
+    {
+
+        StartMenu.GetComponent<Canvas>().enabled = false;
+        instruct.GetComponent<Canvas>().enabled = true;
+        particleDot.SetActive(false);
+        Anim1 = Title.GetComponent<Animator>();
+        Anim2 = Starts.GetComponent<Animator>();
+        Anim3 = particleDot.GetComponent<Animator>();
+        Anim3.SetBool("OnDot", false);
+        Anim1.SetBool("OnTitle", false);
+        Anim2.SetBool("OnStart", false);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+     
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        StartMenu.GetComponent<Canvas>().enabled = true;
+        instruct.GetComponent<Canvas>().enabled = false;
+        particleDot.SetActive(true);
+        Anim3.SetBool("OnDot", true);
+        Anim1.SetBool("OnTitle", true);
+        Anim2.SetBool("OnStart", true);
+
+
+    }
+}
