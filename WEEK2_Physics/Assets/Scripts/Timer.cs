469e5d8fb1075e7315553445b147857820bab422
Hilary Chan <hilary0310@gmail.com>
2019-09-07 21:39:42 -0400

    100%

---
+++ WEEK2_Physics/Assets/Scripts/Timer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+    public GameObject box;
+    public float timer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Time.timeScale = 1;
+        //timer += Time.deltaTime;
+        timer = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.D)) 
+        {
+            box.SetActive(false);
+        }
+
+        //timer += Time.deltaTime;
+        //if (timer > 1)
+        //{
+        //    Time.timeScale = 1;
+        //}
+    }
+}
